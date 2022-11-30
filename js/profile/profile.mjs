f3a1db2647a56b33dc7c8492baf90d4981ef6ce7
Jonast96 <Jon.Astveit@hotmail.com>
2022-11-30 07:40:12 +0100

    user can now update avatar

--- js/profile/profile.mjs
+++ js/profile/profile.mjs
@@ -11,12 +11,23 @@ logOutFunctions()
 const userName = document.querySelector(".username")
 const credits = document.querySelector(".current_credits")
 const auctions = document.querySelector(".current_auctions")
+const changeAvatar = document.querySelector(".change_avatar")
+const changeAvatarInput = document.querySelector(".change_avatar_input")
+const userImage = document.querySelector(".img_container_profile")
+
+
+changeAvatar.addEventListener("click", () => {
+
+    changeAvatarInput.classList.replace("d-none", "d-flex")
+})
+
 
 const userToken = localStorage.getItem("userToken")
 const localStorageUserName = localStorage.getItem("userName")
 const baseUrl = "https://nf-api.onrender.com"
 const profileUrl = `${baseUrl}/api/v1/auction/profiles/${localStorageUserName}?_listings=true`
-
+const updateUrl = `${baseUrl}/api/v1/auction/profiles/${localStorageUserName}/media
+`
 
 redirectIfNotLoggedIn(userToken)
 
@@ -36,9 +47,45 @@ async function getUser() {
     userName.innerHTML = `Hello ${json.name}!`
     credits.innerHTML = `${json.credits}`
     auctions.innerHTML = `${json._count.listings}`
+    userImage.innerHTML = `
+    <img
+    class="img-fluid rounded-circle"
+    style="width:200px;height:200px;object-fit:cover;"
+    src="${json.avatar}"
+    alt=""
+  />
+    `
 
 
 }
-
 getUser()
 
+
+
+async function putUser(url, updatedAvatar) {
+    const postData = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${userToken}`
+        },
+        body: JSON.stringify(updatedAvatar)
+    }
+    const response = await fetch(url, postData)
+    const json = await response.json()
+    console.log(json)
+}
+
+
+function updatedAvatarFunction() {
+    const updatedAvatarInput = document.querySelector(".updated_avatar_input")
+    console.log(updatedAvatarInput.value)
+    const newAvatar = { avatar: `${updatedAvatarInput.value}` }
+
+    putUser(updateUrl, newAvatar)
+}
+
+const avatarButton = document.querySelector(".avatar_button")
+avatarButton.addEventListener("click", () => {
+    updatedAvatarFunction()
+})
