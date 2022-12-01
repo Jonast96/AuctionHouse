1707e8edbb504966ec6a6fa3c455eb44790f457c
Jonast96 <Jon.Astveit@hotmail.com>
2022-12-01 06:31:29 +0100

    testing

--- js/index/index.mjs
+++ js/index/index.mjs
@@ -7,29 +7,28 @@ const getUrl = `${baseUrl}/api/v1/auction/listings/?_bids=true`
 
 
 async function getCall(url) {
-    const postData = {
-        headers: {
-            "Content-Type": "application/json"
-        },
-    }
-    const response = await fetch(url, postData)
-    const json = await response.json()
-    console.log(json[0])
+  const postData = {
+    headers: {
+      "Content-Type": "application/json"
+    },
+  }
+  const response = await fetch(url, postData)
+  const json = await response.json()
+  // console.log(json)
 
 
-    const bidClosingTime = json[5].endsAt
-    console.log(bidClosingTime)
+  const event = new Date(json[4].created);
+  const options = { hour: 'numeric' };
+  const time = event.toLocaleDateString("en-us", options)
 
 
-    const event = new Date(json[4].created);
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric' };
 
-    const time = event.toLocaleDateString("en-us", options)
-    console.log(time)
 
-    for (let i = 0; i < 1; i++) {
-        createHtml(json[i])
-    }
+
+
+  for (let i = 0; i < 10; i++) {
+    createHtml(json[i], time)
+  }
 
 }
 
@@ -39,23 +38,29 @@ const container = document.querySelector(".auctions_container")
 
 
 
-function createHtml(json) {
+function createHtml(json, time) {
 
 
-    container.innerHTML += `          <div
-class="row mt-5 border border-2 shadow-lg border-secondary rounded container m-auto p-2"
+  const lastItem = json.bids.length - 1
+  console.log(lastItem)
+
+  console.log(json.bids[`${lastItem}`].amount)
+
+
+  container.innerHTML += `          <div
+class="row mt-5 bg-light border-bottom shadow-bottom border-secondary rounded container m-auto p-2"
 >
 <div class="col-12 col-md-4 col-lg-4 col-sm-12">
   <div class="img_all_container">
     <img
       class="img_all rounded"
-      src="/images/hasbuuuuuuuuuulla.jpg"
+      src="${json.media[0]}"
       alt=""
     />
   </div>
 </div>
 <div class="col-sm-12 col-md-8 col-lg-6">
-  <h4 class="font-monospace">Bid closing in: 01:42:12</h4>
+  <h5 class="">Bid closing: ${time}</h5>
   <h3>${json.title}</h3>
   <p>
   ${json.description}
@@ -66,7 +71,7 @@ class="row mt-5 border border-2 shadow-lg border-secondary rounded container m-a
     <h3 class="text-center">Current price</h3>
   </div>
   <div>
-    <h3 class="text-center font-monospace">$${json.bids.pop().amount}</h3>
+    <h3 class="text-center ">$${json.bids.amount}</h3>
   </div>
   <div class="d-flex justify-content-center">
     <button class="btn btn-info">View auction</button>
